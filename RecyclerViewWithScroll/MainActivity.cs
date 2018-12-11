930e5169a4f64ebae6619384060e0e1d8f5a0f09
techno1d <techno1d@yandex.ru>
2018-12-11 14:46:07 +0300

    init
    init

--- RecyclerViewWithScroll/MainActivity.cs
+++ RecyclerViewWithScroll/MainActivity.cs
@@ -3,8 +3,10 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Support.V7.Widget;
+using Android.Views;
 
-namespace RecyclerViewWithScroll
+namespace NestedRecyclerView
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
@@ -14,6 +16,48 @@ namespace RecyclerViewWithScroll
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-        }
-    }
+					   
+			var recyclerView = FindViewById<RecyclerView>(Resource.Id.list);
+			recyclerView.SetAdapter(new Adapter());
+			recyclerView.SetLayoutManager(new LinearLayoutManager(this));
+		}
+
+		private class Adapter : RecyclerView.Adapter
+		{
+			private enum ViewType
+			{
+				Dummy = 1,
+				Input = 2
+			}
+
+			private int totalCount = 25;
+			private int inputPosition = 8;
+			
+			public override int ItemCount => totalCount;
+
+			public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+			{
+				return;
+			}
+
+			public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+			{
+				var view = LayoutInflater.From(parent.Context).Inflate(viewType == 1 ? Resource.Layout.item_dummy : Resource.Layout.input_item, parent, false);
+				ViewHolder vh = new ViewHolder(view);
+
+				return new ViewHolder(view);
+			}
+
+			public override int GetItemViewType(int position)
+			{
+				if (position == inputPosition) return (int)ViewType.Input;
+				else return (int)ViewType.Dummy;
+			}
+		}
+
+		private class ViewHolder : RecyclerView.ViewHolder
+		{
+			public ViewHolder(View view) : base(view) { }
+		}
+	}
 }
---
+++ RecyclerViewWithScroll/NestedRecyclerView.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+using Android.Support.V7.Widget;
+using Android.Support.V4.View;
+
+namespace NestedRecyclerView
+{
+	[Register("com.techno1d.NestedRecyclerView")]
+	public class NestedRecyclerView : RecyclerView, INestedScrollingParent
+	{
+		protected Context _context;
+
+		private View _nestedScrollTarget = null;
+		private bool _nestedScrollTargetIsBeingDragged = false;
+		private bool _nestedScrollTargetWasUnableToScroll = false;
+		private bool _skipsTouchInterception = false;
+		
+		#region Ctors
+		public NestedRecyclerView(Context context) : base(context)
+		{
+			_context = context;
+		}
+
+		public NestedRecyclerView(Context context, IAttributeSet attrs) : base(context, attrs) { _context = context; }
+
+		public NestedRecyclerView(Context context, IAttributeSet attrs, int defStyle) : base(context, attrs, defStyle)
+		{
+			_context = context;
+		}
+
+		protected NestedRecyclerView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
+		#endregion
+
+
+		public override bool DispatchTouchEvent(MotionEvent e)
+		{
+			bool temporarilySkipsInterception = _nestedScrollTarget != null;
+			if (temporarilySkipsInterception)
+			{
+				// If a descendent view is scrolling we set a flag to temporarily skip our onInterceptTouchEvent implementation
+				_skipsTouchInterception = true;
+			}
+
+			// First dispatch, potentially skipping our onInterceptTouchEvent
+			bool handled = base.DispatchTouchEvent(e);
+
+			if (temporarilySkipsInterception)
+			{
+				_skipsTouchInterception = false;
+
+				// If the first dispatch yielded no result or we noticed that the descendent view is unable to scroll in the
+				// direction the user is scrolling, we dispatch once more but without skipping our onInterceptTouchEvent.
+				// Note that RecyclerView automatically cancels active touches of all its descendents once it starts scrolling
+				// so we don't have to do that.
+				if (!handled || _nestedScrollTargetWasUnableToScroll)
+				{
+					handled = base.DispatchTouchEvent(e);
+				}
+			}
+
+			return handled;			
+		}
+
+		// Skips RecyclerView's onInterceptTouchEvent if requested
+		
+		public override bool OnInterceptTouchEvent(MotionEvent e)
+		{
+			return base.OnInterceptTouchEvent(e) && !_skipsTouchInterception;
+		}
+
+		public override void OnNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed)
+		{
+			base.OnNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+			if (target == _nestedScrollTarget && !_nestedScrollTargetIsBeingDragged)
+			{
+				if (dyConsumed != 0)
+				{
+					// The descendent was actually scrolled, so we won't bother it any longer.
+					// It will receive all future events until it finished scrolling.
+					_nestedScrollTargetIsBeingDragged = true;
+					_nestedScrollTargetWasUnableToScroll = false;
+				}
+				else if (dyConsumed == 0 && dyUnconsumed != 0)
+				{
+					// The descendent tried scrolling in response to touch movements but was not able to do so.
+					// We remember that in order to allow RecyclerView to take over scrolling.
+					_nestedScrollTargetWasUnableToScroll = true;
+					if (target.Parent != null)
+						target.Parent.RequestDisallowInterceptTouchEvent(false);
+				}
+			}
+			
+		}
+
+		public override void OnNestedScrollAccepted(View child, View target, [GeneratedEnum] ScrollAxis axes)
+		{
+			if (axes != 0 && Android.Views.ScrollAxis.Vertical != 0)
+			{
+				// A descendent started scrolling, so we'll observe it.
+				_nestedScrollTarget = target;
+				_nestedScrollTargetIsBeingDragged = false;
+				_nestedScrollTargetWasUnableToScroll = false;
+			}
+						
+			base.OnNestedScrollAccepted(child, target, axes);
+		}
+
+		public override bool OnStartNestedScroll(View child, View target, [GeneratedEnum] ScrollAxis nestedScrollAxes)
+		{
+			bool secondPart = (int)Build.VERSION.SdkInt < 21 || ScrollAxis.Vertical != 0;
+
+			return (nestedScrollAxes != 0 && secondPart);
+		}
+
+		public override void OnStopNestedScroll(View child)
+		{
+			_nestedScrollTarget = null;
+			_nestedScrollTargetIsBeingDragged = false;
+			_nestedScrollTargetWasUnableToScroll = false;
+		}		
+	}
+}
